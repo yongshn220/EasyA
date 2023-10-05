7ff030a6efcb6e0b0bb6130a75cce92b9c7e3985
Yongjung Shin <yongshn220@gmail.com>
2023-10-05 16:22:12 -0400

    Adjust depends on viewport size

--- src/components/Rank.js
+++ src/components/Rank.js
@@ -1,11 +1,50 @@
 import Grid from "@mui/material/Grid";
 import BasicCard from "./BasicCard";
 import Box from "@mui/material/Box";
+import {useEffect, useState} from "react";
 
 export default function Rank({title, avgData, rankType}) {
+  const [itemsPerRow, setItemsPerRow] = useState(1);
+
   const scoreTitle = (rankType === "Grade")? "A: " : "0-3h: "
   const dataKey = (rankType === "Grade")? "A" : "0-3"
-  console.log(avgData);
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Define the breakpoints and the number of items per row for each breakpoint
+      const breakpoints = [
+        { breakpoint: 700, items: 1 },
+        { breakpoint: 1000, items: 2 },
+        { breakpoint: 1200, items: 3 },
+        { breakpoint: 1400, items: 4 },
+        { breakpoint: 1920, items: 5 },
+      ];
+
+      // Determine the number of items per row based on the window width
+      for (const breakpoint of breakpoints) {
+        if (window.innerWidth < breakpoint.breakpoint) {
+          console.log(window.innerWidth)
+          setItemsPerRow(breakpoint.items);
+          return;
+        }
+      }
+
+      // Default to 5 items per row if none of the breakpoints match
+      setItemsPerRow(5);
+    };
+
+    // Initial call to set the initial number of items per row
+    handleResize();
+
+    // Listen for the window resize event
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
       <Box style={{display:'flex', justifyContent:'center', alignItems:'center', flex: "0 0 50px", fontSize:20, fontWeight:'700'}}>
@@ -15,7 +54,7 @@ export default function Rank({title, avgData, rankType}) {
         <Grid container spacing={1}>
           {
             avgData.map((data, index) => (
-              <Grid item xs={12 / 5} key={index}>
+              <Grid item xs={12 / itemsPerRow} key={index}>
                 <BasicCard rank={index+1} name={data.name} score={scoreTitle + data[rankType][dataKey] + "%"}/>
               </Grid>
             ))
