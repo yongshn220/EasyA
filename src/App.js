08664578d9062d5b6dc2ac17cbfa33cd69b7d1b4
Yongjung Shin <yongshn220@gmail.com>
2023-10-04 22:41:01 -0400

    remove | unuse lib

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 import Home from './pages/Home'
 import {RecoilRoot} from "recoil";
-import {Suspense} from "react";
 
 function App() {
   return (
