68655c23c5ea66080d6accda16882a110b6b5dc8
Yongjung Shin <yongshn220@gmail.com>
2023-11-05 03:57:34 -0500

    minor change

--- src/pages/DayOff/components/DayOffBanner.js
+++ src/pages/DayOff/components/DayOffBanner.js
@@ -6,7 +6,7 @@ export default function DayOffBanner() {
 
   return (
     <Box style={{flex: 1}}>
-      <Box style={{margin:10, fontSize:'10rem', fontWeight:'700'}}>Make a <span style={{ color: COLOR.yellow }}>Day-Off</span></Box>
+      <Box style={{padding:10, fontSize:'10rem', fontWeight:'700'}}>Make a <span style={{ color: COLOR.yellow }}>Day-Off</span></Box>
       <Box style={{margin:20, fontSize:'2.0rem', fontWeight:'700',}}>Find easy courses in the specific time!</Box>
       <Box style={{position:'relative', margin:20, fontSize:'1.2rem', fontWeight:'700', color:"#b2b2b2"}}>
         <InfoIcon style={{paddingRight: 5}}/>
