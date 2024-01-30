c8a9ffca3e3af43dd1b6f01e31cf5611abae9583
Yongjung Shin <yongshn220@gmail.com>
2024-01-30 15:18:10 -0500

    update

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,3 @@
 
-// export const serverURI = process.env.REACT_APP_API_SERVER
-export const serverURI = "http://0.0.0.0:8000"
+export const serverURI = process.env.REACT_APP_API_SERVER
+// export const serverURI = "http://0.0.0.0:8000"
